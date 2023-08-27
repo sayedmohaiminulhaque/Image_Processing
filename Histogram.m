83598c36b29c6c344886a609d397b26137809828
Sayed Mohaiminul Haque <118075159+sayedmohaiminulhaque@users.noreply.github.com>
2023-08-27 14:58:28 +0600

    Update Histogram.m

--- Histogram.m
+++ Histogram.m
@@ -3,16 +3,19 @@ clear all;
 close all;
 img=imread("images/cameraman.tif");
 for i=1:256
-   k(i)=0; 
+   M(2,i)=0; 
 end
 for l=1:256
+    M(1,l)=l-1;
 for i=1:256
   for j=1:256
-      if img(i,j)==l 
-          k(l)=k(l)+1;
+      if img(i,j)==l-1 
+          M(2,l)=M(2,l)+1;
       end
   end
 end
 end
+
 imshow(img);
+%imhist(img);
 
